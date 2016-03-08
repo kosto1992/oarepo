66f50d08e7aefd7467eadf094d02a884a72111fc
lukas kostensky <kostelu1@fit.cvut.cz>
2016-03-08 14:43:05 +0100

    Romiste index is now showing items from the individual repos

--- romiste/view.py
+++ romiste/view.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from romiste.models import ScientistPerson, RomanyPerson, Place, RomanyThing
+
 
 def index(req):
+    scientists = ScientistPerson.objects.all()
+    respondents = RomanyPerson.objects.all()
+    places = Place.objects.all()
+    recordings = RomanyThing.objects.all()
     return render(req, 'romiste/index.html', {
+        'scientists' : scientists,
+        'respondents' : respondents,
+        'places' : places,
+        'recordings' : recordings
     })
