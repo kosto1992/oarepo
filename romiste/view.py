8dba31d1be637b799e5b68931865b284e7550f70
Miroslav Simek <miroslav.simek@gmail.com>
2016-03-08 17:29:10 +0100

    showng last 10 items on roma homepage

--- romiste/view.py
+++ romiste/view.py
@@ -4,10 +4,10 @@ from romiste.models import ScientistPerson, RomanyPerson, Place, RomanyThing
 
 
 def index(req):
-    scientists = ScientistPerson.objects.all()
-    respondents = RomanyPerson.objects.all()
-    places = Place.objects.all()
-    recordings = RomanyThing.objects.all()
+    scientists = ScientistPerson.objects.all().order_by('-_fedora_last_modified')[:10]
+    respondents = RomanyPerson.objects.all().order_by('-_fedora_last_modified')[:10]
+    places = Place.objects.all().order_by('-_fedora_last_modified')[:10]
+    recordings = RomanyThing.objects.all().order_by('-_fedora_last_modified')[:10]
     return render(req, 'romiste/index.html', {
         'scientists' : scientists,
         'respondents' : respondents,
