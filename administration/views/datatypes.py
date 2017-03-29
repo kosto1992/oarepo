c35b7268eaa775591abec1b849dc8bb7435d9169
Mirek Simek <miroslav.simek@vscht.cz>
2017-03-29 15:00:49 +0200

    Added a _meta field "application" with the name of the enclosing application

--- administration/views/datatypes.py
+++ administration/views/datatypes.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 
 from fedoralink.type_manager import FedoraTypeManager
+from fedoralink_ui.models import ResourceType, ResourceFieldType
 
 
 class IndexView(TemplateView):
@@ -10,6 +11,16 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         for model in FedoraTypeManager.models:
             print('model:', model)
+            print("    application: ", model._meta.application)
+            print("    rdf types: ", model._meta.rdf_types)
+            print("    fields: ")
             for field in model._meta.fields:
-                print('    %s (%s)' % (field.name, field.rdf_name))
+                print('        %s (%s)' % (field.name, field.rdf_name))
+
+        for rt in ResourceType.objects.all():
+            print("resource type: ", rt.label)
+            print("    view template", rt.template_view)
+            print("    edit template", rt.template_edit)
+            print("    list item template", rt.template_list_item)
+
         return context
